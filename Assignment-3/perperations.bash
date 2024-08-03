6bdd758522b2d3dd3e46d6c8c84187afc0f67a2c
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-03 16:51:59 -0400

    Create perperations.bash

---
+++ Assignment-3/perperations.bash
@@ -0,0 +1,14 @@
+########
+
+
+hadoop@ashraf-VirtualBox:~$ hdfs dfs -mkdir /user/airlines
+hadoop@ashraf-VirtualBox:~$ hdfs dfs -mkdir /user/flights
+hadoop@ashraf-VirtualBox:~$ hdfs dfs -mkdir /user/airports
+hadoop@ashraf-VirtualBox:~$ hdfs dfs -put /home/hadoop/Downloads/airlines.csv /user/airlines
+hadoop@ashraf-VirtualBox:~$ hdfs dfs -put /home/hadoop/Downloads/flights.csv /user/flights
+hadoop@ashraf-VirtualBox:~$ hdfs dfs -put /home/hadoop/Downloads/airports.csv /user/airports
+
+
+
+
+#######
