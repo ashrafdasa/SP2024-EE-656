5ce5b33724e897d2133022d9e5c56da001366626
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-03 17:35:52 -0400

    Update preparations.bash

--- Assignment-3/preparations.bash
+++ Assignment-3/preparations.bash
@@ -12,3 +12,18 @@ hadoop@ashraf-VirtualBox:~$ hdfs dfs -put /home/hadoop/Downloads/airports.csv /u
 
 
 #######
+a. Count the number of rows in each table.
+b. Display the first 10 records from each table.
+c. Find the total departure delay for each airline.
+d. Find the top 5 flights with the longest arrival delays.
+e. List the top 5 busiest airports by the number of departures.
+f. Find the total departure delay for each airline, along with the airline name.
+g. List the top 5 busiest airports by number of departures, along with airport
+names.
+h. Find the top 5 flights with the longest arrival delays, including airline name and
+destination airport name.
+i. Identify the airline with the most flights arriving at a specific airport.
+j. List the top 5 airlines with the highest total arrival delays at a specific airport
+(e.g., ORD).
+3. Answer questions a, e, f, h, and I, from question 3 above, using map-reduce running on
+hadoop.
