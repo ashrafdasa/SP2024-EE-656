b5e5e32659b6bdc2890f285d0f1fbdc52dae76fe
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-03 20:22:31 -0400

    Update e.sql

--- Assignment-3/e.sql
+++ Assignment-3/e.sql
@@ -1,4 +1,15 @@
 
+hive>set hiveconf:hive.cli.print.current.db=true;
+hive (default)> show databases;
+OK
+airlineanalytics
+default
+Time taken: 0.276 seconds, Fetched: 2 row(s)
+hive (default)> use airlineanalytics;
+OK
+Time taken: 0.058 seconds
+hive (airlineanalytics)> 
+
 
 
 
