1bc002ee95e565a3da50ddb6278e21d5e65cf93d
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-06 01:41:09 -0400

    Create h.sql

---
+++ Assignment-3/h.sql
@@ -0,0 +1,18 @@
+
+hive>set hiveconf:hive.cli.print.current.db=true;
+hive (default)> show databases;
+OK
+airlineanalytics
+default
+Time taken: 0.276 seconds, Fetched: 2 row(s)
+hive (default)> use airlineanalytics;
+OK
+Time taken: 0.058 seconds
+hive (airlineanalytics)> 
+
+
+
+
+
+########################  h.  Find the top 5 flights with the longest arrival delays, including airline name and destination airport name.   ############
+  
