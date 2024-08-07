a3f480f8e613a3912c0eb4d011a4f966107de281
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-07 13:32:19 -0500

    Update map-reduce_hadoop_a.py

--- Assignment-3/map-reduce_hadoop_a.py
+++ Assignment-3/map-reduce_hadoop_a.py
@@ -1,1 +1,57 @@
 
+
+
+
+
+######################### a. Count the number of rows in each table. ##############################
+ 
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import numpy as np
+
+class solution(MRJob):  
+    def steps(self):
+        return [
+            MRStep(
+                mapper=self.mapper,
+                reducer=self.reducer), 
+            
+            MRStep(
+                mapper=self.mapper2,
+                reducer=self.reducer2), 
+            ]
+    def mapper(self, _, line):
+        uniqId = "0"
+        tbleName = ""
+        fields = line.split('\t') 
+        if(len(fields) == 4): 
+            tbleName = "A" #"u.data"
+            uniqId = fields[0] 
+            yield uniqId,(tbleName,1)
+        else:
+            fields = line.split('|') 
+            tbleName = "B"  # u.user tbl
+            gender = fields[2]
+            uniqId = fields[0]
+            if(gender == 'F'):  
+                yield uniqId,(tbleName,gender)
+         
+    def reducer(self, key, values): 
+        records = list(values)
+        a_records = [v for v in records if v[0] == 'A']
+        b_records = [v for v in records if v[0] == 'B'] 
+        for a_record in a_records: # Perform the right join
+            if b_records:
+                for b_record in b_records:
+                    yield key, (str(a_record[1]) + b_record[1])
+            
+            
+    def mapper2(self, id, name):  
+        yield name,1 
+    def reducer2(self, key, values):   
+        yield key, sum(values) 
+    
+
+if __name__ == '__main__': 
+    solution.run()  
+    
