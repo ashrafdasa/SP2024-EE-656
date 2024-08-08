30fdba6b1391b10624bf4e77b9ad68ef644f3cf3
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-07 23:10:53 -0400

    Update map-reduce_hadoop_a.py

--- Assignment-3/map-reduce_hadoop_a.py
+++ Assignment-3/map-reduce_hadoop_a.py
@@ -1,8 +1,5 @@
 
 
-
-
-
 ######################### a. Count the number of rows in each table. ##############################
   
 from mrjob.job import MRJob
@@ -23,4 +20,37 @@ class solution(MRJob):
  
 
 if __name__ == '__main__': 
-    solution.run()  
+    solution.run()
+
+
+
+
+--------------------------------------------
+
+
+
+hadoop@ashraf-VirtualBox:~/pywork/mapperWork/assignment-3$ /bin/python3 /home/hadoop/pywork/mapperWork/assignment-3/map-reduce_assignment-3-MR.py
+No configs found; falling back on auto-configuration
+No configs specified for inline runner
+Creating temp directory /tmp/map-reduce_hadoop_a.hadoop.20240808.030640.678152
+Running step 1 of 1...
+job output is in /tmp/map-reduce_hadoop_a.hadoop.20240808.030640.678152/output
+Streaming final output from /tmp/map-reduce_hadoop_a.hadoop.20240808.030640.678152/output...
+0       15
+Removing temp directory /tmp/map-reduce_hadoop_a.hadoop.20240808.030640.678152...
+No configs found; falling back on auto-configuration
+No configs specified for inline runner
+Creating temp directory /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.201272
+Running step 1 of 1...
+job output is in /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.201272/output
+Streaming final output from /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.201272/output...
+0       323
+Removing temp directory /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.201272...
+No configs found; falling back on auto-configuration
+No configs specified for inline runner
+Creating temp directory /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.831610
+Running step 1 of 1...
+job output is in /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.831610/output
+Streaming final output from /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.831610/output...
+0       5819080
+Removing temp directory /tmp/map-reduce_hadoop_a.hadoop.20240808.030641.831610...
