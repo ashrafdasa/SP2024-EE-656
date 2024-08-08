77fdfbf6964879759f5664c291c62a0eea83cf1c
Ashraf Dasa <71946494+ashrafdasa@users.noreply.github.com>
2024-08-07 23:09:24 -0400

    Update map-reduce_assignment-3-MR.py

--- Assignment-3/map-reduce_assignment-3-MR.py
+++ Assignment-3/map-reduce_assignment-3-MR.py
@@ -1,20 +1,27 @@
+ 
+import os  
+withHadoop =  " -r hadoop"
 
-import os
-os.chdir("pywork/mapperWork/assignment-2/")
-withHadoop =   "-r hadoop"
 
 # a. Count the number of rows in each table.
-shellCommand = "python3 map-reduce_hadoop_a.py u.item "+ withHadoop  
+shellCommand = "python3 map-reduce_hadoop_a.py airlines.csv "+ withHadoop  
+os.system(shellCommand)
+
+shellCommand = "python3 map-reduce_hadoop_a.py airports.csv "+ withHadoop  
+os.system(shellCommand)
+
+shellCommand = "python3 map-reduce_hadoop_a.py flights.csv "+ withHadoop  
 os.system(shellCommand)
 
 
 # e. List the top 5 busiest airports by the number of departures.
-shellCommand = "python3 map-reduce_hadoop_e.py u.item "+ withHadoop  
-os.system(shellCommand)
+shellCommand = "python3 map-reduce_hadoop_e.py flights.csv "+ withHadoop  
+# os.system(shellCommand)
 
-# f. Find the total departure delay for each airline, along with the airline name.
-shellCommand = "python3 map-reduce_hadoop_f.py u.item "+ withHadoop  
-os.system(shellCommand)
+#  f. Find the total departure delay for each airline, along with the airline name. 
+shellCommand = "python3 map-reduce_hadoop_f.py flights.csv airlines.csv"+ withHadoop  
+#os.system(shellCommand)
+
 
 # h. Find the top 5 flights with the longest arrival delays, including airline name and destination airport name.
 shellCommand = "python3 map-reduce_hadoop_h.py u.item "+ withHadoop  
@@ -24,3 +31,5 @@ os.system(shellCommand)
 # i. Identify the airline with the most flights arriving at a specific airport.
 shellCommand = "python3 map-reduce_hadoop_i.py u.item "+ withHadoop  
 os.system(shellCommand)
+
+ 
